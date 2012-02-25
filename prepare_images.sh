40996cccd30249e0cda3158240c8e7d03f1a8ea2
Peter Hancock <peter.hancock@gmail.com>
2012-02-25 09:52:32 +0000

    Better title positioning

--- prepare_images.sh
+++ prepare_images.sh
@@ -1,2 +1,2 @@
-convert -crop 800x200+0+0 images/background.png images/background_title.png 
-convert -crop 800x800+0+200 images/background.png images/background_india.png
+convert -crop 800x220+0+0 images/background.png images/background_title.png 
+convert -crop 800x780+0+220 images/background.png images/background_india.png
